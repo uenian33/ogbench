806fcd9aebf32a0facd9cc140a23a7a50abb4e15
uenian33 <44872697+uenian33@users.noreply.github.com>
2025-10-28 15:19:07 +0200

    update

--- impls/train_rws_triton_all.sh
+++ impls/train_rws_triton_all.sh
@@ -7,10 +7,10 @@ MAX_PARALLEL="${1:-6}"
 # We have 25 tasks: indices 0..24
 ARRAY_RANGE="0-24%${MAX_PARALLEL}"
 
-mkdir -p /scratch/work/yangw4/ogbench/logs
+mkdir -p /scratch/work/yangw4/ogbench/impls/logs
 
 # Submit without pinning a partition; Triton will auto-pick a GPU partition
-sbatch --array="${ARRAY_RANGE}" train_ogbench_array.sbatch
+sbatch --array="${ARRAY_RANGE}" train_rws_triton_sub.sbatch
 
 echo "Submitted array ${ARRAY_RANGE}. View queue with:  squeue -u $USER"
 
